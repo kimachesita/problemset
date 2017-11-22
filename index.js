bb8bd9f82f089440d8bdc4e7d6b732106ec396aa
kimachesita <kimachesita@gmail.com>
2017-11-22 05:58:15 +0000

    feat(solution): Solution to Problem 1

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // solutions here
+'use strict'
+const asyncOp = require('./lib/lib.js').asyncOp;
+
+// Solution for #1. Asynchronous Operations
+function doAsync(input){
+    let cur_elem = input.splice(0,1)[0],
+    len = cur_elem.length;
+
+    for(let elem of cur_elem){
+        asyncOp(elem).then(()=>{
+            len--;
+            if(input.length === 0){
+                return;
+            }
+            if(len === 0){
+                doAsync(input);
+            }
+
+        });
+    }
+} 
+
+module.exports = {doAsync};
