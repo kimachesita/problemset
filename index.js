6b231d128490796a26b34f5bc16b3a9cdbb61a10
kimachesita <kimachesita@gmail.com>
2017-11-23 00:24:49 +0000

    feat(Solution): Added some comments on solution

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const asyncOp = require('./lib/lib.js').asyncOp;
 
 // Solution for #1. Asynchronous Operations
 function doAsync(input){
-    let cur_elem = input.splice(0,1)[0],
-    len = cur_elem.length;
+    let cur_elem = input.splice(0,1)[0],    // get first element of input
+    len = cur_elem.length;                  // determines if the element is a single value
 
-    for(let elem of cur_elem){
-        asyncOp(elem).then(()=>{
-            len--;
+    // iterate on each value of the first element
+    for(let val of cur_elem){
+        asyncOp(val).then(()=>{
+            len--;                          //tracks the number of promises done on a parallel execution
             if(input.length === 0){
-                return;
+                return;                     
             }
             if(len === 0){
-                doAsync(input);
+                doAsync(input);             //proceed to next element of input if completed
             }
-
         });
     }
 } 
