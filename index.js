54ed9c625746d3abb9ad6c20842105985475554a
kimachesita <kimachesita@gmail.com>
2017-11-30 06:12:29 +0000

    feat(solution): Added solution for #3 Resource Pooling

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ const events = require('events');
 
 // Solution for #1. Asynchronous Operations
 function doAsync(input){
-    let cur_elem = input.splice(0,1)[0],    // get first element of input
-    len = cur_elem.length;                  // determines if the element is a single value
+    let cur_elem = input.splice(0,1)[0],                    // get first element of input
+    len = cur_elem.length;                                  // determines if the element is a single value
 
     // iterate on each value of the first element
     for(let val of cur_elem){
         asyncOp(val).then(()=>{
-            len--;                          //tracks the number of promises done on a parallel execution
+            len--;                                          //tracks the number of promises done on a parallel execution
             if(input.length === 0){
                 return;                     
             }
             if(len === 0){
-                doAsync(input);             //proceed to next element of input if completed
+                doAsync(input);                             //proceed to next element of input if completed
             }
         });
     }
@@ -46,7 +46,68 @@ class RandStringSource extends events.EventEmitter{
     }
 }
 
+//solution for #3 Resource Pooling
+
+//A resource object class
+class ResourceObject extends events.EventEmitter {
+    constructor(){
+        super();
+        this.available = true;
+    }
+
+    reserve(){
+        this.available = false;
+        return this;
+    }
+
+    release(){
+        this.available = true;
+        this.emit('release');
+    }
+}
+
+class ResourceManager{
+    constructor(count){
+        // create and initialize internal arrays
+        // taskQueue - waiting list for unentertained callbacks
+        // resources - an array container of all object resources
+        this.taskQueue = [];
+        this.resources = [];
+        //create resource objects base on count
+        for(let i=0;i<count;i++){
+            let resource = new ResourceObject(`obj${i}`);
+            //listen for finished task
+            resource.on('release',()=>{
+                //execute 1st waiting task on the queue
+                //if no more task do nothing
+                if(this.taskQueue.length != 0) this.taskQueue.splice(0,1)[0](resource);
+            });
+            this.resources.push(resource);
+        }
+    }
+
+    borrow(callback){
+        //find available resource objects first
+        //if resource available execute callback
+        //if non, put callback to waiting queue
+        let obj = this._findAvailable();
+        if(obj){
+            callback(obj.reserve());
+        }else{
+            this.taskQueue.push(callback);
+        }
+    }   
+
+    _findAvailable(){
+        for(let obj of this.resources){
+            if(obj.available) return obj;
+        }
+        return null;
+    }
+}
+
 module.exports = {
     doAsync,
-    RandStringSource
+    RandStringSource,
+    ResourceManager
 };
